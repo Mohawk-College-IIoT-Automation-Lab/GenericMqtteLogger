83f969f00f3932c6a694753c1618db09bd4b372b
Gabe Casciano <gabecasciano@gmail.com>
2025-04-30 13:22:17 -0400

    added david's loggger and adapted it

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from .generic_mqtt import GenericMQTT
-from .logger import LoggerSingleton, Logger
 
-__all__ = ["GenericMQTT", "LoggerSingleton", "Logger"]
+__all__ = ["GenericMQTT"]
 
 __version__ = "0.1.0"
 __author__ = "Gabriel Casciano"
--- generic_mqtt.py
+++ generic_mqtt.py
@@ -1,15 +1,12 @@
 from paho.mqtt.client import Client, MQTTMessage
-from .logger import LoggerSingleton, Logger
+from .davids_logger import initialize_logging
+import logging
 
 class GenericMQTT:
 
-    def __init__(self, host_name:str="localhost", host_port:int = 1883, logger:Logger=None):
+    def __init__(self, host_name:str="localhost", host_port:int = 1883):
         
-        if logger is not None:
-            self.logger = logger.logger
-        else:
-            self.logger = LoggerSingleton().logger
-        self.logger.debug("Creating GenericMQTT object")
+        initialize_logging("Generic Mqtt", host_name, host_port)
 
         self._connected = False
         self._host_name = host_name
@@ -32,25 +29,25 @@ class GenericMQTT:
             self.mqtt_client.loop_stop()
 
         # Loggign and emit status
-        self.logger.info(f"[MQTT] Connection status: {self.connected}")
+        logging.info(f"[MQTT] Connection status: {self.connected}")
         
     def _mqtt_failed(self):
         self._connected = self.mqtt_client.is_connected()
         self.mqtt_client.loop_stop()
-        self.logger.error("[MQTT] Failed to connect")
+        logging.error("[MQTT] Failed to connect")
 
     def _mqtt_default_callback(self, client:Client, userdata, message:MQTTMessage):
-        self.logger.info(f"[MQTT] unhandled data received from topic: {message.topic} -> {message.payload.decode()}")
+        logging.info(f"[MQTT] unhandled data received from topic: {message.topic} -> {message.payload.decode()}")
 
     def mqtt_connect(self):
-        self.logger.info(f"[MQTT] Attempting connection to host: {self._host_name} on port: {self._host_port}")
+        logging.info(f"[MQTT] Attempting connection to host: {self._host_name} on port: {self._host_port}")
         
         error = self.mqtt_client.connect(self._host_name, self._host_port)
         if error:
-            self.logger.error(f"[MQTT] Error connecting to host: {self._host_name}:{self._host_port}, error code: {error}")
+            logging.error(f"[MQTT] Error connecting to host: {self._host_name}:{self._host_port}, error code: {error}")
             
     def mqtt_disconnect(self):
-        self.logger.info(f"Disconnecting from {self._host_name}:{self._host_port}")
+        logging.info(f"Disconnecting from {self._host_name}:{self._host_port}")
         self.mqtt_client.disconnect()
 
     @property
@@ -64,7 +61,7 @@ class GenericMQTT:
     @host_name.setter
     def host_name(self, value: str):
         if self.connected:
-            self.warning("Cannot change host name while connected")
+            logging.warning("Cannot change host name while connected")
             return
         self._host_name = value
 
@@ -75,7 +72,7 @@ class GenericMQTT:
     @host_port.setter
     def host_port(self, value: int):
         if self.connected:
-            self.warning("Cannot change host port while connected")
+            logging.warning("Cannot change host port while connected")
             return
         self._host_port = value
 
